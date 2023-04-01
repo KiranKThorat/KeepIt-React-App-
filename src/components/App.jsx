9ff96a12c4b7e7c1a28d8d80e9784738f2ab94b4
kt <kthorat158@gmail.com>
2023-04-01 23:30:41 +0530

    keepit app part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
 
 function App(){
     return (
     <div>
     <Header />
-    <Note />
+    {notes.map((notedata) =>
+        <Note 
+        key = {notedata.id}
+        title = {notedata.title}
+        content = {notedata.content}
+        />
+    )}
     <Footer />
     </div>
     ); 
